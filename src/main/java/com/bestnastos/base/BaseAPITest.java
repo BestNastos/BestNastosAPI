1d565ce0197155cd137f7cd4c32a0a1ad2dde04e
Anastasia <anastasia.ermushina@kronos.com>
2021-12-29 20:55:50 +0300

    delete comment (!)

--- src/main/java/com/bestnastos/base/BaseAPITest.java
+++ src/main/java/com/bestnastos/base/BaseAPITest.java
@@ -41,16 +41,5 @@ public class BaseAPITest {
                 .setBaseUri(BASE_URI)
                 .build();
     }
-
-        /*
-    public APIResponse createShiftByShiftTemplate(Map<String, Object> dataMap) throws KronosCoreAPIException {
-        setGenericParameter();
-        params.setHeader(DYNATRACE_HEADER, DYNATRACE_HEADER_BASE_VALUE + "createShiftByShiftTemplate");
-        String payload = build(dataMap).getRequest().toString();
-        RequestSpecification requestSpec = given().cookie(driver.getCookies()).accept(ContentType.JSON)
-                .headers(params.getHeader()).contentType(ContentType.JSON).when().body(payload)
-                .pathParameters(params.getPath()).queryParameters(params.getQuery());
-        Response response = requestSpec.post(BACKEND_SERVER + CREATE_BY_TEMPLATE);
-        return new APIResponse(response);
-    }*/
+    //delete comment
 }
